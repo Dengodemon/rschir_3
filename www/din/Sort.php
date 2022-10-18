823a424715bf1ac77ce4e884fb720945aac33d5e
Dengodemon <95023540+Dengodemon@users.noreply.github.com>
2022-10-18 10:10:04 +0300

    Add files via upload

---
+++ www/din/Sort.php
@@ -0,0 +1,34 @@
+<html lang="en">
+<head>
+<title>Hello world page</title>
+    <link rel="stylesheet" href="style.css" type="text/css"/>
+</head>
+<body>
+    <div>
+<?php
+function quick_sort($my_array)
+ {
+	$loe = $gt = array();
+	if(count($my_array) < 2)
+	{
+		return $my_array;
+	}
+	$pivot_key = key($my_array);
+	$pivot = array_shift($my_array);
+	foreach($my_array as $val)
+	{
+		if($val <= $pivot)
+		{
+			$loe[] = $val;
+		}elseif ($val > $pivot)
+		{
+			$gt[] = $val;
+		}
+	}
+	return array_merge(quick_sort($loe),array($pivot_key=>$pivot),quick_sort($gt));
+}             
+echo implode(", ", quick_sort(explode(",", $_GET["array"])));
+        ?>
+         </div>
+</body>
+</html>
